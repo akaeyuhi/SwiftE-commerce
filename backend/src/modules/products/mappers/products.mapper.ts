4151d274b73d0433fdfb64f1dbcc988357a0dc47
akaeyuhi <andrvostr0101@gmail.com>
2025-07-07 17:24:22 +0300

    feat: basic product module classes

---
+++ backend/src/modules/products/mappers/products.mapper.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { BaseMapper } from 'src/common/abstracts/base.mapper';
+import { Product } from 'src/entities/product.entity';
+import { CreateProductDto } from 'src/modules/products/dto/create-product.dto';
+
+@Injectable()
+export class ProductsMapper extends BaseMapper<Product, CreateProductDto> {
+  toDto(entity: Product): CreateProductDto {
+    return {
+      id: entity.id,
+      name: entity.name,
+      description: entity.description,
+      storeId: entity.store?.id,
+      categoryId: entity.category?.id,
+      createdAt: entity.createdAt,
+      updatedAt: entity.updatedAt,
+    };
+  }
+
+  toEntity(dto: Partial<CreateProductDto>): Product {
+    const product = new Product();
+    product.id = dto.id!;
+    product.name = dto.name!;
+    product.description = dto.description;
+    return product;
+  }
+}
--- backend/src/modules/products/products.module.ts
+++ backend/src/modules/products/products.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from 'src/entities/product.entity';
+import { ProductRepository } from './products.repository';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
+import { ProductsMapper } from './mappers/products.mapper';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Product])],
+  providers: [ProductRepository, ProductsService, ProductsMapper],
   controllers: [ProductsController],
-  providers: [ProductsService],
 })
 export class ProductsModule {}
