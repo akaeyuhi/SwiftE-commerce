717deb3199ec9abfe20fc1ccafc14933c8f59f48
akaeyuhi <andrvostr0101@gmail.com>
2025-07-07 17:57:58 +0300

    fix: db config file

--- backend/src/database/database.module.ts
+++ backend/src/database/database.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { DatabaseConnectionConfiguration } from 'ormconfig';
+import { DatabaseConnectionConfiguration } from 'src/config/ormconfig';
 
 @Module({
   imports: [TypeOrmModule.forRoot(DatabaseConnectionConfiguration)],
