53ca75fb4bab0f891d8e12c3899c8bf377091c0c
akaeyuhi <andrvostr0101@gmail.com>
2025-07-14 16:34:14 +0300

    feat: enum for storing permissions

---
+++ backend/src/common/enums/resources.enum.ts
@@ -0,0 +1,12 @@
+export enum ResourcesEnum {
+  AUTH = 'auth',
+  USERS = 'users',
+  ORDERS = 'orders',
+  CART = 'cart',
+  CATEGORIES = 'categories',
+  INVENTORY = 'inventory',
+  NEWS = 'news',
+  PRODUCTS = 'products',
+  REVIEWS = 'reviews',
+  STORES = 'stores',
+}
