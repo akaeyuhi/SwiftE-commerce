eb02087c30a0780ba164a9a6db7ab6067f393230
akaeyuhi <andrvostr0101@gmail.com>
2025-08-15 20:57:06 +0300

    feat: simple jwt auth strategy

---
+++ backend/src/modules/auth/strategies/auth-jwt.strategy.ts
@@ -0,0 +1,28 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { UsersService } from '../../users/users.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private config: ConfigService,
+    private userService: UsersService
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: config.get('JWT_SECRET')!,
+    });
+  }
+
+  async validate(payload: any) {
+    // payload contains { id, email, sub }
+    const user = await this.userService.findOneWithRelations(payload.id);
+    if (!user) throw new UnauthorizedException();
+    // attach computed permissions & storeRole arrays if needed in user object
+    // e.g., user.permissions = computePermissionsFromRoles(user.roles)
+    return user;
+  }
+}
