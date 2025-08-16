fcc9360db5c5c11ef989e8c50d280696929e93fb
akaeyuhi <andrvostr0101@gmail.com>
2025-08-16 20:07:45 +0300

    fix: fixed auth strategy name

--- backend/src/modules/auth/strategies/auth-jwt.strategy.ts
+++ backend/src/modules/auth/strategies/auth-jwt.strategy.ts
@@ -5,7 +5,7 @@ import { ConfigService } from '@nestjs/config';
 import { UserService } from 'src/modules/user/user.service';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
+export class AuthJwtStrategy extends PassportStrategy(Strategy) {
   constructor(
     private config: ConfigService,
     private userService: UserService
@@ -13,7 +13,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: config.get('JWT_SECRET')!,
+      secretOrKey: config.get<string>('JWT_SECRET')!,
     });
   }
 
