df9b2909b0519fe84d5300e1d162e3de0948e964
akaeyuhi <andrvostr0101@gmail.com>
2025-09-04 14:25:29 +0300

    fix: delete unused permission scope

--- backend/src/common/decorators/permissions.decorator.ts
+++
@@ -1,6 +0,0 @@
-import { Permission } from 'src/common/interfaces/permission.interface';
-import { SetMetadata } from '@nestjs/common';
-
-export const PERMISSIONS_KEY = 'permissions';
-export const Permissions = (...perms: Permission[]) =>
-  SetMetadata(PERMISSIONS_KEY, perms);
