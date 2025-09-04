344cc25e050f66818baf2414456cb7690fd8e993
akaeyuhi <andrvostr0101@gmail.com>
2025-09-04 18:44:38 +0300

    fix: fixed type annotations

--- backend/src/common/interfaces/crud.interface.ts
+++ backend/src/common/interfaces/crud.interface.ts
@@ -1,7 +1,14 @@
-export interface ICrudService<Entity, CreateDto, UpdateDto> {
-  findAll(): Promise<Entity[]>;
-  findOne(id: string): Promise<Entity>;
-  create(dto: CreateDto): Promise<Entity>;
-  update(id: string, dto: UpdateDto): Promise<Entity>;
+import { ObjectLiteral } from 'typeorm';
+
+export interface ICrudService<
+  Entity extends ObjectLiteral,
+  CreateDto = Partial<Entity>,
+  UpdateDto = Partial<Entity>,
+  TransferDto = Partial<Entity>,
+> {
+  findAll(): Promise<Entity[] | TransferDto[]>;
+  findOne(id: string): Promise<Entity | TransferDto>;
+  create(dto: CreateDto): Promise<Entity | TransferDto>;
+  update(id: string, dto: UpdateDto): Promise<Entity | TransferDto>;
   remove(id: string): Promise<void>;
 }
