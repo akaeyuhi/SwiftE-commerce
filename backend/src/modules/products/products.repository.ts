c6261d58cf41f8eaee942fe76549f9af090d997a
akaeyuhi <andrvostr0101@gmail.com>
2025-09-04 15:19:04 +0300

    feat: basic methods for products repo

--- backend/src/modules/products/products.repository.ts
+++ backend/src/modules/products/products.repository.ts
@@ -1,10 +1,26 @@
 import { Injectable } from '@nestjs/common';
-import { DataSource, Repository } from 'typeorm';
+import { DataSource } from 'typeorm';
 import { Product } from 'src/entities/product.entity';
+import { BaseRepository } from 'src/common/abstracts/base.repository';
 
 @Injectable()
-export class ProductRepository extends Repository<Product> {
+export class ProductRepository extends BaseRepository<Product> {
   constructor(dataSource: DataSource) {
     super(Product, dataSource.createEntityManager());
+  }
+
+  async findWithRelations(id: string): Promise<Product | null> {
+    return this.findOne({
+      where: { id },
+      relations: ['variants', 'photos', 'category', 'store'],
+    });
+  }
+
+  async findAllByStore(storeId: string, page = 1, limit = 50) {
+    return this.find({
+      where: { store: { id: storeId } },
+      take: limit,
+      skip: (page - 1) * limit,
+    });
   }
 }
