11518583ca1447e11af97c4f21f4ce5da704c930
akaeyuhi <andrvostr0101@gmail.com>
2025-09-05 18:18:12 +0300

    feat: create and update methods for BaseRepo

--- backend/src/common/abstracts/base.repository.ts
+++ backend/src/common/abstracts/base.repository.ts
@@ -1,17 +1,32 @@
-import { ObjectLiteral, Repository } from 'typeorm';
+import { DeepPartial, ObjectLiteral, Repository } from 'typeorm';
+import { NotFoundException } from '@nestjs/common';
 
 export abstract class BaseRepository<
-  T extends ObjectLiteral,
-> extends Repository<T> {
-  async findAll(): Promise<T[]> {
+  Entity extends ObjectLiteral,
+> extends Repository<Entity> {
+  async findAll(): Promise<Entity[]> {
     return this.find();
   }
 
-  async findById(id: string): Promise<T | null> {
+  async findById(id: string): Promise<Entity | null> {
     return this.findOneBy({ id } as any);
   }
 
   async deleteById(id: string): Promise<void> {
     await this.delete(id);
+  }
+
+  async createEntity(data: DeepPartial<Entity>): Promise<Entity> {
+    const entity = this.create(data);
+    return await this.save(entity);
+  }
+
+  async updateEntity(id: string, data: Partial<Entity>): Promise<Entity> {
+    const admin = await this.findById(id);
+    if (!admin) {
+      throw new NotFoundException(`Entity with ID ${id} not found`);
+    }
+    await this.update(id, data);
+    return admin;
   }
 }
