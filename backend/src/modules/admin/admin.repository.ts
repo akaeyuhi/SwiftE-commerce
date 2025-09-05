92daab1e2eb529b8d83cf4061b45055f98a3fcf7
akaeyuhi <andrvostr0101@gmail.com>
2025-09-05 18:17:52 +0300

    feat: basic admin module for creating and managing admins

---
+++ backend/src/modules/admin/admin.repository.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { DataSource } from 'typeorm';
+import { BaseRepository } from 'src/common/abstracts/base.repository';
+import { Admin } from 'src/entities/admin.entity';
+
+@Injectable()
+export class AdminRepository extends BaseRepository<Admin> {
+  constructor(dataSource: DataSource) {
+    super(Admin, dataSource.createEntityManager());
+  }
+}
---
+++ backend/src/modules/admin/admin.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { BaseService } from 'src/common/abstracts/base.service';
+import { Admin } from 'src/entities/admin.entity';
+import { AdminRepository } from 'src/modules/admin/admin.repository';
+
+@Injectable()
+export class AdminService extends BaseService<Admin> {
+  constructor(private readonly adminRepo: AdminRepository) {
+    super(adminRepo);
+  }
+
+  async findByUserId(userId: string): Promise<Admin | null> {
+    return await this.repository.findOne({ where: { user: { id: userId } } });
+  }
+
+  async isUserValidAdmin(userId: string): Promise<boolean> {
+    return !!(await this.findByUserId(userId));
+  }
+}
