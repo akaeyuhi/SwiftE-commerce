3d93cd3730ad37d946f4bde386a568f92a1dd92a
akaeyuhi <andrvostr0101@gmail.com>
2025-09-05 18:16:55 +0300

    feat: module imports

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { ProductPhotoModule } from './modules/product-photo/product-photo.module';
+import { AdminModule } from './modules/admin/admin.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), ProductPhotoModule],
+  imports: [ConfigModule.forRoot(), ProductPhotoModule, AdminModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -8,6 +8,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { RefreshToken } from 'src/entities/refresh-token.entity';
 import { RefreshTokenModule } from 'src/modules/refresh-token/refresh-token.module';
 import { ConfigModule } from '@nestjs/config';
+import { PolicyService } from './policy/policy.service';
+import { AdminModule } from 'src/modules/admin/admin.module';
+import { UserModule } from 'src/modules/user/user.module';
 
 @Module({
   imports: [
@@ -15,8 +18,15 @@ import { ConfigModule } from '@nestjs/config';
     TypeOrmModule.forFeature([RefreshToken]),
     RefreshTokenModule,
     ConfigModule,
+    AdminModule,
+    UserModule,
   ],
   controllers: [AuthController],
-  providers: [AuthService, AuthJwtStrategy, RefreshTokenStrategy],
+  providers: [
+    AuthService,
+    AuthJwtStrategy,
+    RefreshTokenStrategy,
+    PolicyService,
+  ],
 })
 export class AuthModule {}
