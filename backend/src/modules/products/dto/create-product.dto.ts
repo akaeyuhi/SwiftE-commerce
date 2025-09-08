622d56ec95f1811d324b233aea3c44e0dc17251e
akaeyuhi <andrvostr0101@gmail.com>
2025-09-08 19:11:55 +0300

    feat: added comments to dto

--- backend/src/modules/products/dto/create-product.dto.ts
+++ backend/src/modules/products/dto/create-product.dto.ts
@@ -1,8 +1,20 @@
-import { IsOptional, IsString, IsUUID } from 'class-validator';
+import { IsString, IsOptional, IsUUID } from 'class-validator';
 
 export class CreateProductDto {
-  @IsString() name: string;
-  @IsString() @IsOptional() description?: string;
-  @IsUUID() storeId: string;
-  @IsString() categoryId?: string;
+  @IsString()
+  name: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsUUID()
+  storeId: string;
+
+  /**
+   * Optional category id (UUID) to assign product to a category.
+   */
+  @IsOptional()
+  @IsUUID()
+  categoryId?: string;
 }
