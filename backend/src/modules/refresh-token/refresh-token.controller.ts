26950957d7b2611bf7eed3647ad6dfb0933e6dcf
akaeyuhi <andrvostr0101@gmail.com>
2025-09-08 22:56:48 +0300

    fix: deleted unnecessary controller

--- backend/src/modules/refresh-token/refresh-token.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { RefreshTokenService } from './refresh-token.service';
-
-@Controller()
-export class RefreshTokenController {
-  constructor(private readonly refreshTokenService: RefreshTokenService) {}
-}
