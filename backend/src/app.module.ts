de7e9239c792f2161c3a52d4f51c3d0888aae41b
akaeyuhi <andrvostr0101@gmail.com>
2025-09-15 15:30:28 +0300

    fix: path fixes

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { ProductPhotoModule } from './modules/product-photo/product-photo.module';
+import { ProductPhotoModule } from 'src/modules/products/product-photo/product-photo.module';
 import { AdminModule } from './modules/admin/admin.module';
 
 @Module({
