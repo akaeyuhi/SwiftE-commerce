21983bf5dc5a503f50f726e6d01263ef882d594f
akaeyuhi <andrvostr0101@gmail.com>
2025-09-23 00:34:20 +0300

    feat: added basic swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,11 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  const config = new DocumentBuilder()
+    .setTitle('ESwift API')
+    .setDescription('The ESwift API 1.0 basic example and description')
+    .setVersion('1.0')
+    .addTag('e-commerce')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
