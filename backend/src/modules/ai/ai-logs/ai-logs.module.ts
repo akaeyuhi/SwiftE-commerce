2fa9640e94cab325ac97aae4cd40c8bab6fd8782
akaeyuhi <andrvostr0101@gmail.com>
2025-09-22 23:14:45 +0300

    fix: import fix

--- backend/src/modules/ai/ai-logs/ai-logs.module.ts
+++ backend/src/modules/ai/ai-logs/ai-logs.module.ts
@@ -4,9 +4,10 @@ import { AiLogsController } from 'src/modules/ai/ai-logs/ai-logs.controller';
 import { AiLogRepository } from 'src/modules/ai/ai-logs/ai-log.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AiLog } from 'src/entities/ai/ai-log.entity';
+import {PolicyModule} from "src/modules/auth/policy/policy.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AiLog])],
+  imports: [TypeOrmModule.forFeature([AiLog]), PolicyModule],
   controllers: [AiLogsController],
   providers: [AiLogsService, AiLogRepository],
   exports: [AiLogsService, AiLogRepository],
