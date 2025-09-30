e1ee782000430e7d3946c147edf2c132d989ecda
akaeyuhi <andrvostr0101@gmail.com>
2025-09-30 19:53:12 +0300

    feat: new dto for analytics

---
+++ backend/src/modules/analytics/dto/index.ts
@@ -0,0 +1,5 @@
+export * from './analytics-query.dto';
+export * from './batch-events.dto';
+export * from './aggregation-request.dto';
+export * from './record-event.dto';
+export * from './get-stats.dto';
